90848989ce603ccd43c0077b7377ff9c087c8aaa
Tobias Kauer <tobias.kauer@fh-potsdam.de>
2023-03-23 13:53:16 +0100

    add https support

--- server/server.js
+++ server/server.js
@@ -1,34 +1,59 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
 const bodyParser = require("body-parser");
-//const multer = require("multer");
+
+const dotenv = require("dotenv");
+const https = require('https');
+const http = require('https');
+const fs = require('fs');
+dotenv.config()
+
+const credentials = {} 
+const ENVIRONMENT = process.env.ENVIRONMENT || 'local';
+if(ENVIRONMENT == "STAGING" || ENVIRONMENT == "PRODUCTION") {
+  const CREDDIR = process.env.CREDDIR
+  
+  const privateKey = fs.readFileSync(CREDDIR+'privkey.pem', 'utf8');
+  const certificate = fs.readFileSync(CREDDIR+'cert.pem', 'utf8');
+  const ca = fs.readFileSync(CREDDIR+'chain.pem', 'utf8');
+
+	credentials.key = privateKey,
+	credentials.cert = certificate,
+	credentials.ca = ca
+}
 
 const app = express();
 
-var corsOptions = {
-  origin: "http://localhost:3000"
-};
 
+const CORS = process.env.CORS || 'http://localhost';
+var corsOptions = {
+  origin: CORS
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//use multer to store POST images to /static and serve them there
-//app.use(multer({dest:'./static'}).single('image'))
-//app.use('/static', express.static('static'))
 
 require("./routes/trace.routes")(app);
 
 
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
 
+const PORT = process.env.PORT || 8080;
 const db = require("./models");
+
+if(credentials.cert) {
+  
+  var httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(PORT);
+  console.log(`HTTPS is running on port ${PORT}.`);
+} else {
+  var httpServer = http.createServer(app);
+httpServer.listen(PORT);
+console.log(`HTTPS is running on port ${PORT}.`);
+}
+
+
 
 db.sequelize.sync(
   //{force:true} //dont use force in prod
